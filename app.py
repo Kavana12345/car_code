c2e40ecc1f22788cf6edd4945fb06938230b0b6f
Kavana12345 <57221633+Kavana12345@users.noreply.github.com>
2021-05-21 13:58:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template,request
 from datetime import date
 import pickle
 
-app=Flask(__name__,static_url_path='/static')
+app=Flask(__name__)
 
 model=pickle.load(open('modfile1.pkl','rb'))
 
